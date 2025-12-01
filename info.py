2944c7b258ac95a0e0dbe11073fec2f71b9aa25d
tamilvig <tamiltunemix@gmail.com>
2025-12-01 10:28:06 +0530

    Update TUTORIAL link to new URL

--- info.py
+++ info.py
@@ -70,7 +70,7 @@ MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "")
 # Verification
 IS_VERIFY = is_enabled("IS_VERIFY", True)
 # ---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/cinemacityhub_info")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/")
 VERIFY_IMG = environ.get(
